4e1befd5c9d8015ea4cd83b14b91d21af093bc82
Andrey Kuzmin <andrey.qr@mail.ru>
2023-09-26 22:32:57 +0300

    new tables

---
+++ V2.0__createNewTables.sql
@@ -0,0 +1,47 @@
+create table orders
+(
+	order_id bigint,
+	status text,
+	delivery_address text,
+	start_price float,
+	final_price float,
+	registration_date date
+);
+create table user_order
+(
+	user_id bigint,
+	order_id bigint
+);
+create table bonusAccounts
+(
+	bonusAccount_id bigint,
+	user_id bigint,
+	bonuses bigint
+);
+create table order_bonusAccount
+(
+	order_id bigint,
+	bonusAccount_id bigint,
+	bonuses bigint,
+	withdrawal_date date
+);
+create table couriers
+(
+	courier_id bigint,
+	first_name varchar(30),
+	last_name varchar(30)
+);
+create table delivery
+(
+	delivery_id bigint,
+	status text,
+	courier_id bigint,
+	delivery_date date,
+	type_delivery text
+);
+create table order_delivery
+(
+	order_id bigint,
+	delivery_id bigint,
+	list_products text
+);
