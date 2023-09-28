8c954c108f04cc15f15e8c559f868b46404b8eff
Islam Bagaviev <67591516+Verdan-blip@users.noreply.github.com>
2023-09-28 22:29:11 +0300

    Added next tables
    Added tables:
    -payments,
    -purchases,
    -paid_products,
    -warehouses,
    -producer,
    -replenishments,
    -products_warehouses,
    -products_orders

---
+++ V1.3__createNextTables.sql
@@ -0,0 +1,58 @@
+create table payments
+(
+	payment_id BIGINT,
+	order_id BIGINT,
+	status VARCHAR(30),
+	total FLOAT
+);
+
+create table purchases
+(
+	user_id BIGINT,
+	purchase_id BIGINT
+);
+
+create table paid_products
+(
+	payment_id BIGINT,
+	order_id BIGINT,
+	payment_date DATE
+);
+
+create table warehouses
+(
+	warehouse_id BIGINT,
+	adress VARCHAR(128)
+);
+
+create table producer
+(
+	producer_id BIGINT,
+	producer_name VARCHAR(30),
+	registered_address VARCHAR(128)
+);
+
+create table replenishments
+(
+	warehouse_id BIGINT,
+	producer_id BIGINT,
+	product_id BIGINT,
+	supply_date DATE
+);
+
+
+create table products_warehouses
+(
+	product_id BIGINT,
+	warehouse_id BIGINT,
+	products_amount INT,
+	shabbiness INT
+);
+
+
+create table products_orders
+(
+	order_id BIGINT,
+	product_id BIGINT,
+	product_amount INT
+);
