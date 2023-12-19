edfe642211b0979e77a0186b6e45a4c869f62c88
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2023-12-19 16:28:26 +0300

    now 5_3 is really fixed

--- V5.3__Scripts.sql
+++ V5.3__Scripts.sql
@@ -7,5 +7,5 @@ ALTER TABLE pickuppoint ADD PRIMARY KEY (id);
 ALTER TABLE pickuppoint ALTER COLUMN id SET DEFAULT nextval('pickuppoint_seq');
 
 CREATE SEQUENCE employee_work_in_pickpoint_seq;
-ALTER TABLE employee_work_in_pickpoint_seq ADD PRIMARY KEY (employee_pickpoint_id);
-ALTER TABLE employee_work_in_pickpoint_seq ALTER COLUMN employee_pickpoint_id SET DEFAULT nextval('employee_work_in_pickpoint_seq');
+ALTER TABLE employee_work_in_pickpoint ADD PRIMARY KEY (employee_pickpoint_id);
+ALTER TABLE employee_work_in_pickpoint ALTER COLUMN employee_pickpoint_id SET DEFAULT nextval('employee_work_in_pickpoint_seq');
